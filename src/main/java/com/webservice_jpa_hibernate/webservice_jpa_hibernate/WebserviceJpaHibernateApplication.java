17df3679355dc1d0d35d5d78b40a7d4a13818b3a
Wesley Matos <wesleycostamatos@gmail.com>
2024-07-14 01:04:59 -0300

    Project created

---
+++ src/main/java/com/webservice_jpa_hibernate/webservice_jpa_hibernate/WebserviceJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.webservice_jpa_hibernate.webservice_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservice_jpa_hibernate/webservice_jpa_hibernate/WebserviceJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice_jpa_hibernate.webservice_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
